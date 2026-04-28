971c222422e04c636d2516f46b77b838c785de98
wilfrid <sallesottawilfrid@gmail.com>
2026-04-28 11:38:16 +0200

    Last Modifications

--- src/fonctions/worker/function_app.py
+++ src/fonctions/worker/function_app.py
@@ -3,8 +3,8 @@ import logging
 
 app = func.FunctionApp()
 
-@app.blob_trigger(arg_name="myblob", path="doc-storage/input/{name}",
-                               connection="docstorageprof_STORAGE") 
+@app.blob_trigger(arg_name="myblob", path="doc-storage/{name}",
+                               connection="docstorage") 
 def Test(myblob: func.InputStream):
     logging.info(f"Version CI/CD => Python blob trigger function processed blob"
                 f"Name: {myblob.name}"
